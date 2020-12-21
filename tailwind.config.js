a2ebd114fcda556f45d1a455689892a58dcce5a3
Wladimir Guevara <wladige@gmail.com>
2020-12-20 22:25:47 -0300

    creando proyecto portafolio en html, tailwindcss  js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+      fontFamily:{
+        roboto:['Roboto Slab'],
+      },
+    extend: {
+      colors:{
+        danger:'#ff5f40',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
